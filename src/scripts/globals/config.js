7b01dc074e4f59adfbbe5ec98d9fa664e685fa07
rusdiahamid <rusdia.hamid@gmail.com>
2023-01-13 23:24:05 +0700

    chaneg ekey

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'FJKfw',
+  KEY: process.env.API_KEY_TMDB,
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
